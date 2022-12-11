6fd57f5a5f425967760698979226346a0e38f7f3
afraidias <afraidiasmedina@gmail.com>
2022-12-11 16:07:11 +0100

    feat: :lipstick: add home letter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WebsiteModule } from './website/website.module';
 
 @NgModule({
 	declarations: [AppComponent],
-	imports: [BrowserModule],
+	imports: [BrowserModule, WebsiteModule],
 	providers: [],
 	bootstrap: [AppComponent]
 })
